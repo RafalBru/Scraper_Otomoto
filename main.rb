5eb966a5b37fea7fc2e82b24349506817969bb68
RafalBru <93768384+RafalBru@users.noreply.github.com>
2024-07-31 13:23:11 +0200

    Add files via upload

--- main.rb
+++ main.rb
@@ -3,44 +3,49 @@ require "nokogiri"
 require "csv"
 require_relative "carinfo.rb"
 
-response = HTTParty.get("https://www.otomoto.pl/osobowe?page=1")
 def generate_csv(cars)
-  CSV.open("otomoto.tsv","wb",col_sep:"\t") do |csv|
+  CSV.open("otomoto.csv","w", col_sep: "\t") do |csv|
     csv << ["LINK","MARKA","MODEL","ROK PRODUKCJI","PALIWO","SKRZYNIA BIEGÓW"]
     cars.each do |car|
       fullname = car.nazwa
       marka, model = fullname.split(" ",2)
+      #puts [car.image_url,marka,model,car.rok_produkcji,car.rodzaj_paliwa,car.skrzynia_biegow].inspect
       csv << [car.image_url,marka,model,car.rok_produkcji,car.rodzaj_paliwa,car.skrzynia_biegow]
     end
   end
 end
 
+# response = HTTParty.get("https://www.otomoto.pl/osobowe?page=1")
+# doc = Nokogiri::HTML(response.body)
+# cars = doc.xpath('//section')
 cars_to_sell = []
-doc = Nokogiri::HTML(response.body)
-cars = doc.xpath('//section')
-
-cars.each do |car|
-  image_url = car.at_xpath('.//img')&.[]('src')
-  car_name = car.at_xpath('.//h1//a')&.text&.strip
-  rok_produkcji = car.at_xpath('.//dt[contains(text(),"Rok produkcji")]//following-sibling::dd[1]')&.text&.strip
-  paliwo = car.at_xpath('.//dt[contains(text(),"Rodzaj paliwa")]//following-sibling::dd[1]')&.text&.strip
-  skrzynia_biegow = car.at_xpath('.//dt[contains(text(),"Skrzynia biegów")]//following-sibling::dd[1]')&.text&.strip
-
-  if car_name && rok_produkcji && paliwo && skrzynia_biegow
-    car_info = Carinfo.new(
-      image_url,
-      car_name,
-      rok_produkcji,
-      paliwo,
-      skrzynia_biegow)
+page = 1
+4.times do
+  puts "Page #{page}"
+  response = HTTParty.get("https://www.otomoto.pl/osobowe?page=#{page}")
+  doc = Nokogiri::HTML(response.body)
+  cars = doc.xpath('//section')
+  
+  cars.each do |car|
+    image_url = car.at_xpath('.//img')&.[]('src')
+    car_name = car.at_xpath('.//h1//a')&.text&.strip
+    rok_produkcji = car.at_xpath('.//dt[contains(text(),"Rok produkcji")]//following-sibling::dd[1]')&.text&.strip
+    paliwo = car.at_xpath('.//dt[contains(text(),"Rodzaj paliwa")]//following-sibling::dd[1]')&.text&.strip
+    skrzynia_biegow = car.at_xpath('.//dt[contains(text(),"Skrzynia biegów")]//following-sibling::dd[1]')&.text&.strip
+  
+    if car_name && rok_produkcji && paliwo && skrzynia_biegow
+      car_info = Carinfo.new(
+        image_url,
+        car_name,
+        rok_produkcji,
+        paliwo,
+        skrzynia_biegow)
       cars_to_sell.push(car_info)
+    end
   end
-  # cars_to_sell.each do |car|
-  #   car.print_info
-  # end
-  generate_csv(cars_to_sell)
-
+  page += 1
 end
+generate_csv(cars_to_sell)
 # url -> <img>
 # cars.each do |car|
 # image_url = car.at_xpath('.//img')
